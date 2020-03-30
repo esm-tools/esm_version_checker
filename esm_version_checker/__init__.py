907f74b8280cf3111d9798b14de6c152f5aacf8f
Paul Gierz <pgierz@awi.de>
2020-03-30 11:55:45 +0200

    Bump version: 3.0.2 → 3.0.3

--- esm_version_checker/__init__.py
+++ esm_version_checker/__init__.py
@@ -1,5 +1,5 @@
 """esm_version_checker - Mini package to check versions of diverse esm_tools software"""
 
-__version__ = '3.0.2'
+__version__ = '3.0.3'
 __author__ = 'Paul Gierz <pgierz@awi.de>'
 __all__ = []
