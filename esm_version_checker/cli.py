1f6925d1c1407696f092f884fceb66a07fcd8619
Paul Gierz <pgierz@awi.de>
2020-04-02 13:44:14 +0200

    black code style and alais for esm_versions

--- esm_version_checker/cli.py
+++ esm_version_checker/cli.py
@@ -12,28 +12,30 @@ import click
 import esm_rcfile
 
 esm_tools_modules = [
-        "esm_archiving",
-        "esm_autotests",
-        "esm_calendar",
-        "esm_database",
-        "esm_environment",
-        "esm_master",
-        "esm_parser",
-        "esm_profile",
-        "esm_rcfile",
-        "esm_runscripts",
-        "esm_tools",
-        "esm_version_checker",
+    "esm_archiving",
+    "esm_autotests",
+    "esm_calendar",
+    "esm_database",
+    "esm_environment",
+    "esm_master",
+    "esm_parser",
+    "esm_profile",
+    "esm_rcfile",
+    "esm_runscripts",
+    "esm_tools",
+    "esm_version_checker",
 ]
 
 esm_tools_installed = {tool: False for tool in esm_tools_modules}
 
+
 @click.group()
 def main(args=None):
     """Console script for esm_archiving."""
-    #help_message = "Please use the subcommands check or update"
-    #click.echo(help_message)
+    # help_message = "Please use the subcommands check or update"
+    # click.echo(help_message)
     return 0
+
 
 @main.command()
 def check(args=None):
@@ -53,7 +55,7 @@ def check(args=None):
                 try:
                     print(tool, ":", pkg_resources.get_distribution(tool).version)
                 except:
-                    #print("Oops! %s has no version??" % tool)
+                    # print("Oops! %s has no version??" % tool)
                     raise
         else:
             print(tool, ": unknown version!")
@@ -64,46 +66,75 @@ def check(args=None):
 def dist_is_editable(dist):
     """Is distribution an editable install?"""
     for path_item in sys.path:
-        egg_link = os.path.join(path_item, dist.replace("_", "-") + '.egg-link')
+        egg_link = os.path.join(path_item, dist.replace("_", "-") + ".egg-link")
         if os.path.isfile(egg_link):
             return True
     return False
 
+
 def pip_install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "git+https://gitlab.awi.de/esm_tools/"+package])
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "git+https://gitlab.awi.de/esm_tools/" + package,
+        ]
+    )
+
 
 def pip_uninstall(package):
     subprocess.check_call([sys.executable, "-m", "pip", "uninstall", package])
 
+
 def pip_upgrade(package):
     if not dist_is_editable(package):
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "git+https://gitlab.awi.de/esm_tools/"+package])
+        subprocess.check_call(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--upgrade",
+                "git+https://gitlab.awi.de/esm_tools/" + package,
+            ]
+        )
     else:
-        print(package, "is installed in editable mode! No upgrade performed. You may consider doing a git pull here:")
+        print(
+            package,
+            "is installed in editable mode! No upgrade performed. You may consider doing a git pull here:",
+        )
         package = importlib.import_module(package)
         print("/".join(package.__file__.split("/")[:-2]))
 
+
 def pip_or_pull(tool):
     if tool == "esm_tools":
-        print("Special case for esm_tools...")
         FUNCTION_PATH = esm_rcfile.get_rc_entry("FUNCTION_PATH")
         esm_tools_dir = os.path.dirname(FUNCTION_PATH)
         esm_tools_repo = Repo(esm_tools_dir)
         try:
             assert not esm_tools_repo.is_dirty()
         except AssertionError:
-            print("You're esm_tools directory is not clean!")
-            print("Please make sure you check in and commit everything before proceeding!")
+            print("Your esm_tools directory is not clean!")
+            print(
+                "Please make sure you check in and commit everything before proceeding!"
+            )
             raise
         try:
             assert esm_tools_repo.active_branch.name in ["release", "develop"]
             remote = esm_tools_repo.remote()
             remote.pull()
+            print("Pulled new version of ", tool)
         except AssertionError:
-            print("Only allowed to pull on release or develop! Otherwise, do it yourself please...")
+            print(
+                "Only allowed to pull on release or develop! Otherwise, do it yourself please..."
+            )
             raise
     else:
         pip_upgrade(tool)
+
 
 def check_importable_tools():
     for tool in esm_tools_modules:
@@ -114,9 +145,12 @@ def check_importable_tools():
         except ImportError:
             import_successful = False
 
+
 @main.command()
 @click.argument("tool_to_upgrade", default="all")
 def upgrade(tool_to_upgrade="all"):
+    if tool_to_upgrade == "esm_versions":
+        tool_to_upgrade = "esm_version_checker"
     check_importable_tools()
     if tool_to_upgrade == "all":
         for tool in esm_tools_modules:
