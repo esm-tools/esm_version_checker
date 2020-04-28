8281df7deb6dcc77ae7b31df75404f4474e1a0ad
Dirk Barbi <dirk.barbi@awi.de>
2020-04-28 11:31:21 +0200

    Bump version: 3.2.4 → 4.0.0

--- esm_version_checker/__init__.py
+++ esm_version_checker/__init__.py
@@ -1,5 +1,5 @@
 """esm_version_checker - Mini package to check versions of diverse esm_tools software"""
 
-__version__ = "3.2.4"
+__version__ = "4.0.0"
 __author__ = "Paul Gierz <pgierz@awi.de>"
 __all__ = []
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="esm_version_checker",
-    version="3.2.4",
+    version="4.0.0",
     url="https://gitlab.awi.de/esm_tools/esm_version_checker",
     license='MIT',
 
