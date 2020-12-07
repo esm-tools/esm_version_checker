372b18cff62a9b3b2bdda041c86a02ef496554b9
Paul Gierz <pgierz@awi.de>
2020-12-07 11:14:41 +0100

    Bump version: 5.0.0 → 5.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="esm_version_checker",
-    version="5.0.0",
+    version="5.1.0",
     url="https://github.com/esm-tools/esm_version_checker",
     license="MIT",
     author="Paul Gierz",
