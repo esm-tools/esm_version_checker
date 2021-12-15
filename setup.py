28ce5f6fc87bcacb4fa45fbe215c6715ceaa4554
denizural <denizural86@gmail.com>
2021-12-15 10:56:51 +0100

    Bump version: 5.1.6 → 5.1.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="esm_version_checker",
-    version="5.1.6",
+    version="5.1.7",
     url="https://github.com/esm-tools/esm_version_checker",
     license="MIT",
     author="Paul Gierz",
